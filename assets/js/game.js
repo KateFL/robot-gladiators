a9a25662a45d1770c061b7e8e8675ae34a9af66c
Katherine Libbos <katherine.libbos@gmail.com>
2022-03-22 00:21:01 -0400

    three quarters of the way down 3.1.5, The Console. A Developer's Best Friend

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!")
+// the var can be anything, but best practice is to use something another coder stepping in will understand
+var playerName = window.prompt("What is your robot's name?");
+
+// Note the lack of quotation marks around playerName
+window.alert(playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// this is to "call" the function / make it do something (above function won't do anything if you don't call it)
+
+//fight();
